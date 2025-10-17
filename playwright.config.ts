6d565eae53d777e658c3119ee38ec01910fb29fb
sdhanyanair20 <sdhanya.nair20@gmail.com>
2025-10-17 13:16:48 +0530

    Created POM structure and added test cases for ZeroBank application using Playwright.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -12,9 +13,10 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  //testDir: './tests',
   //testDir: './tests/UI_Test',
   //testDir: './tests/UI_Special_Control',
+  testDir: './tests/ZeroBank_Test_PageObject',
   /* Run tests in files in parallel */
   fullyParallel: true,
   workers: 1,
